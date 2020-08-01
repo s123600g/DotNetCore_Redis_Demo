4edc4f06c9f7deede15b3dfa3bb36ea703a37c6c
李俊諭 JYUN-YU LI <s123600g@gmail.com>
2020-08-02 00:04:53 +0800

    建立專案內容

---
+++ DotNetCore_Redis_Demo/Controllers/CacheController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Collections.Generic;
+
+namespace DotNetCore_Redis_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CacheController : ControllerBase
+    {
+        //using StackExchange.Redis;
+        private readonly IDatabase _database;
+
+        // 在類別初始化建構子 就先建立Redis預設資料
+        public CacheController(IDatabase database)
+        {
+            _database = database;
+
+            // 加入一筆資料 Welcome(Key):Hello World(Value)
+            _database.StringSet("Welcome", "Hello World");
+
+            // 加入一筆資料 Author(Key):Hello Lee(Value)
+            _database.StringSet("Author", "Lee");
+        }
+
+        // GET: api/Cache?key=key
+        // Get 參數最好都要有宣告[FromQuery]去包住，要有明確定義參數類型
+        /// <summary>
+        /// 在Redis查詢取得該筆Key的值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get([FromQuery] string key = "Welcome")
+        {
+            return _database.StringGet(key);
+        }
+
+        // using System.Collections.Generic;
+        // POST: api/Cache
+        // Post 參數最好都要有宣告[FromBody]去包住，[FromBody]對應Form表單或JSON(raw)，要有明確定義參數類型
+        /// <summary>
+        /// 在Redis新增一筆紀錄
+        /// </summary>
+        /// <param name="keyValue"></param>
+        [HttpPost]
+        public void Post([FromBody] KeyValuePair<string, string> keyValue)
+        {
+            // 在Redis是用欄位(Key) = 欄位值(Value)作為資料結構
+            _database.StringSet(keyValue.Key, keyValue.Value);
+        }
+    }
+}
---
+++ DotNetCore_Redis_Demo/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+
+namespace DotNetCore_Redis_Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // 加入AddScoped-使用Redis作為暫存區塊
+            //using StackExchange.Redis;
+            IConnectionMultiplexer redis = ConnectionMultiplexer.Connect("172.17.0.4");
+            services.AddScoped(s => redis.GetDatabase());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
